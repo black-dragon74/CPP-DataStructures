d8c992107fd5e3ddc58238e02f86dd727b13bcfe
black.dragon74 <nickk.2974@gmail.com>
2020-11-03 03:22:01 +0530

    Initial commit
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

---
+++ liberr.h
@@ -0,0 +1,16 @@
+//
+// Created by Nick on 11/3/20.
+//
+
+#ifndef CPP_DATASTRUCTURES_LIBERR_H
+#define CPP_DATASTRUCTURES_LIBERR_H
+
+enum DSReturn {
+    kDSReturnError,
+    kDSReturnSuccess,
+    kDSReturnNoMem,
+};
+
+typedef DSReturn DSReturn;
+
+#endif //CPP_DATASTRUCTURES_LIBERR_H
